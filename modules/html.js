df021239a130b476f4cd2c9059658e747b3f05ae
artinep <randriampenitra@gmail.com>
2025-07-25 14:25:27 +0300

    day 22: wasm script generation

---
+++ modules/html.js
@@ -0,0 +1,5 @@
+class Page {
+	constructor() {
+		this.menu = '<button class="largebutton" id="setMainPage">Main page</button><button class="largebutton" id="setPassword">Update password</button><button class="largebutton" id="setWallet">Wallet</button><button class="largebutton" id="setHistory">History</button><button class="largebutton" id="setPool">Pool</button><button class="largebutton" id="setHelp">Help</button><button class="largebutton" id="setAbout">About</button>';
+	}
+}
